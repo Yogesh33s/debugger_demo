b840b5fa7f559aaae353e7bce351c47dfdc74901
Yogesh <yogeshranwa33@gmail.com>
2026-09-24 19:08:40 +0530

    added

---
+++ app/calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a + b   # BUG: should multiply
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
